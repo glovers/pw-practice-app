ca98ac97dfa7662dfed3fd5a3a1218af3ec85861
glovers <samcglover@icloud.com>
2024-02-08 18:47:47 +1100

    Default setup
    Testing

---
+++ playwright.config.ts
@@ -0,0 +1,102 @@
+import { defineConfig, devices } from '@playwright/test';
+
+
+// // attempting to use playwright
+// const config = {
+//   use: {
+//       screenshot: 'only-on-failure',
+//       video: 'retain-on-failure',
+//   },
+//   reporter: [
+//       ['list'],
+//       ['playwright-qase-reporter',
+//           {
+//               apiToken: '44741ade0ab0135209718f69e5393fae6f454fb043415448cdd6a6aa2fa1f95e',
+//               projectCode: 'DEMO',
+//               basePath: 'https://api.qase.io/v1',
+//               uploadAttachments: true,
+//               runComplete: true,
+//               logging: true,
+//               rootSuiteTitle: 'Playwright tests',
+//               environmentId: 'Test 1'
+//           }],
+//   ],
+// };
+// module.exports = config;
+
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,134 @@
+import {expect, test} from '@playwright/test'
+import exp from 'constants'
+
+
+
+test.beforeEach (async ({page})  => {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('Locator syntax rules', async({page}) => {
+    //by Tag name
+   await page.locator('input').first().click()
+
+    //by Id
+    await page.locator('#inputEmail1').click()
+
+    //by Class value
+    page.locator('.shape-recatangle')
+
+    // by attribute
+    page.locator('[placeholder="Email"]')
+
+    //by Class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    // combine different selectors
+    page.locator('input[placeholder="Email"][nbinput]')
+
+    //Xpath (not recommended)
+    page.locator('//*[@id="inputEmail1"]')
+
+    // by partial text match
+    page.locator(':text("Using")')
+
+    //by exact text match
+    page.locator(':text-is("Using the Grid"')
+
+})
+
+
+test('User facing locators', async({page}) => {
+ await page.getByRole('textbox', {name: "Email"}).first().click()
+ await page.getByRole('button', {name: "Sign in"}).first().click()
+  
+ await page.getByLabel('Email').first().click()
+
+ await page.getByPlaceholder('Jane Doe').click()
+
+ await page.getByText('Using the Grid').click()
+
+ await page.getByTitle('IoT Dashboard').click()
+
+ await page.getByTestId(' ')
+})
+
+test('Locating child elements', async({page}) => {
+ await page.locator('nb-card nb radio :text-is("Option 1")').click()
+
+await page.locator('nb-card').locator('nb-radio').locator('nb-card nb radio :text-is("Option 1")').click()
+
+await page.locator('nb-card').getByRole('button', {name: "sign in"}).first().click()
+
+
+await page.locator('nb-card').nth(3).getByRole('button').click()
+
+})
+
+test ('locating parent elements', async ({page}) => {
+await page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"}).click()
+await page.locator('nb-card', {has: page.locator('#inputEmail1')}).getByRole('textbox', {name: "Email"}).click()
+
+await page.locator('nb-card').filter({hasText: "Basic form"}).getByRole('textbox', {name: "Email"}).click()
+await page.locator('nb-card').filter({has: page.locator('nb-checkbox')}).filter({hasText: "Sign in"}).getByRole('textbox', {name: "Email"}).click()
+
+await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: "Email"}).click()
+
+})
+
+test('Reusing the locators', async({page}) => {
+    const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+    const emailField = basicForm.getByRole('textbox', {name: "Email"})
+
+    await emailField.fill('test@test.com')
+    await basicForm.getByRole('textbox', {name: "Password"}).fill('welcome123')
+    await basicForm.locator('nb-checkbox').click()
+    await basicForm.getByRole('button').click()
+
+    await expect(emailField).toHaveValue('test@test.com')
+})
+
+
+test('extracting values', async ({page}) => {
+// single test value
+const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+const buttonText = await basicForm.locator('button').textContent()
+expect (buttonText).toEqual('Submit')
+
+//all test values
+const allRadioButtonLabels = await page.locator('nb-radio').allTextContents()
+expect (allRadioButtonLabels).toContain("Option 1")
+
+//input value
+const emailField = basicForm.getByRole('textbox', {name: "Email"})
+await emailField.fill('test@test.com')
+const emailValue = await emailField.inputValue()
+expect (emailValue).toEqual('test@test.com')
+
+const placeholderValue = await emailField.getAttribute('placeholder')
+expect (placeholderValue).toEqual('Email')
+
+})
+
+test('Assertions', async ({page}) => {
+const basicFormButton = page.locator('nb-card').filter({hasText: "Basic form"}).locator('button') 
+//General assertions
+//Provide value and will confirm assertion and will not wait for anything
+const value = 5
+expect (value).toEqual(5)
+
+
+const text =  await basicFormButton.textContent()
+expect(text).toEqual("Submit")
+
+//locator assertions
+await expect(basicFormButton).toHaveText('Submit')
+
+//Soft Assertion
+// this will still proceed through steps even if  soft step has failed , it is not a good practice but can help find other validation
+await expect.soft(basicFormButton).toHaveText('Submit5')
+await basicFormButton.click()
+
+})
